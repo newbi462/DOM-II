c5fb2c4ec246bba5e7986f984fe7d78173eb467c
newbi462 <isthisemailfree@gmail.com>
2019-10-29 15:12:12 -0400

    A start but all 10 of these do not seem to apply to ellments on this page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
 // Your code goes here
+
+const mainNav = document.querySelector('.main-navigation');
+
+//  mouseover
+mainNav.addEventListener('mouseover', (event) => {
+  event.target.style.backgroundColor = 'black';
+})
+mainNav.addEventListener('mouseleave', (event) => {
+  event.target.style.backgroundColor = 'white';
+})
+
+//keydown
+
+//  wheel
+mainNav.addEventListener('wheel', (event) => {
+  event.target.style.backgroundColor = 'pink';
+})
+
+const introP = document.querySelector('.intro p');
+
+
+//drag / drop
+// load
+// focus
+
+// resize
+/* Need to do this on the window?*/
+const bodyBox = document.querySelector('body');
+window.addEventListener('resize', () => {
+  bodyBox.style.backgroundColor = 'pink';
+})
+
+// scroll
+// select
+
+
+//  dblclick
+const darkmode = document.querySelector('body');
+darkmode.addEventListener('dblclick', (event) => {
+  event.target.style.backgroundColor = 'black';
+  mainNav.style.backgroundColor = 'black';
+})
+
+
+/*
+
+
+
+
+
+*/
